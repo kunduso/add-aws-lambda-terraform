7e298cd111b95fe3a17cddd6debf670d244bb9ec
Sourav Kundu <skundu.dev@gmail.com>
2024-06-11 12:50:17 -0500

    #10 enable scheduled trigger

--- lambda.tf
+++ lambda.tf
@@ -12,21 +12,21 @@ resource "aws_lambda_function" "lambda_run" {
   runtime       = "python3.8"
 }
 
-# resource "aws_cloudwatch_event_rule" "lambda_trigger" {
-#   name                = "lambda_trigger_rule"
-#   schedule_expression = "rate(10 minutes)"
-# }
+resource "aws_cloudwatch_event_rule" "lambda_trigger" {
+  name                = "lambda_trigger_rule"
+  schedule_expression = "rate(10 minutes)"
+}
 
-# resource "aws_cloudwatch_event_target" "lambda_target" {
-#   rule      = aws_cloudwatch_event_rule.lambda_trigger.name
-#   target_id = "lambda_target"
-#   arn       = aws_lambda_function.lambda_run.arn
-# }
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.lambda_trigger.name
+  target_id = "lambda_target"
+  arn       = aws_lambda_function.lambda_run.arn
+}
 
-# resource "aws_lambda_permission" "allow_cloudwatch" {
-#   statement_id  = "AllowExecutionFromCloudWatch"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.lambda_run.function_name
-#   principal     = "events.amazonaws.com"
-#   source_arn = aws_cloudwatch_event_rule.lambda_trigger.arn
-# }
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_run.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_trigger.arn
+}

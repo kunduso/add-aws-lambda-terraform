1e9249614a0015ac3fa280c96f60d766231d4cf5
Sourav Kundu <skundu.dev@gmail.com>
2024-06-11 07:54:58 -0500

    added log stream

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,5 +1,11 @@
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group
 resource "aws_cloudwatch_log_group" "lambda_log" {
   name              = var.name
   retention_in_days = 365
   kms_key_id        = aws_kms_key.encryption_rest.arn
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_stream
+resource "aws_cloudwatch_log_stream" "lambda_log_stream" {
+  name           = "${var.name}-lambda-log-stream"
+  log_group_name = aws_cloudwatch_log_group.lambda_log.name
 }

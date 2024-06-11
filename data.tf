2724e3ced025612b5fd97cf9f7d66abb72208f2e
Sourav Kundu <skundu.dev@gmail.com>
2024-06-10 21:20:05 -0500

    fixed build failure

--- data.tf
+++ data.tf
@@ -2,5 +2,5 @@ data "aws_caller_identity" "current" {}
 locals {
   principal_root_arn       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
   principal_logs_arn       = "logs.${var.region}.amazonaws.com"
-  cloudwatch_log_group_arn = "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:/${var.name}"
+  cloudwatch_log_group_arn = "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:${var.name}"
 }

1b8a0694908bb1f5b3e54b4980c8f354232d1cb3
Sourav Kundu <skundu.dev@gmail.com>
2024-06-12 07:53:58 -0500

    #3 add log stream

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,6 +1,13 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group
 resource "aws_cloudwatch_log_group" "lambda_log" {
-  name              = var.name
+  name              = "${var.log_group_prefix}${var.name}" #"/aws/lambda/${var.name}"
   retention_in_days = 365
   kms_key_id        = aws_kms_key.encryption_rest.arn
+  # depends_on = [ aws_kms_key.encryption_rest ]
+}
+#
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_stream
+resource "aws_cloudwatch_log_stream" "log_stream" {
+  name           = "${var.name}-lambda-log-stream"
+  log_group_name = aws_cloudwatch_log_group.lambda_log.name
 }

e081e18856c7a5da01d2970be53e596f62275b97
Sourav Kundu <skundu.dev@gmail.com>
2024-11-23 19:11:03 -0600

    removed comment

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,6 +1,6 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group
 resource "aws_cloudwatch_log_group" "lambda_log" {
-  name              = "${var.log_group_prefix}${var.name}" #"/aws/lambda/${var.name}"
+  name              = "${var.log_group_prefix}${var.name}"
   retention_in_days = 365
   kms_key_id        = aws_kms_key.encryption.arn
 }

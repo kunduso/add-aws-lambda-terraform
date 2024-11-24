9687bd592a309f5e446d732b5ad461f49ca01fb8
Sourav Kundu <skundu.dev@gmail.com>
2024-11-23 19:18:04 -0600

    corrected reference to vpc

--- vpc_endpoint.tf
+++ vpc_endpoint.tf
@@ -2,7 +2,7 @@
 resource "aws_security_group" "endpoint_sg" {
   name        = "endpoint_access"
   description = "allow inbound traffic"
-  vpc_id      = aws_vpc.this.id
+  vpc_id      = module.vpc.vpc.id
   tags = {
     "Name" = "${var.name}-endpoint-sg"
   }
@@ -14,27 +14,15 @@ resource "aws_security_group_rule" "ingress_vpc_endpoint" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = [aws_vpc.this.cidr_block]
+  cidr_blocks       = [module.vpc.vpc.cidr_block]
   security_group_id = aws_security_group.endpoint_sg.id
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_endpoint
-resource "aws_vpc_endpoint" "secrets_manager" {
-  vpc_id              = aws_vpc.this.id
-  service_name        = "com.amazonaws.${var.region}.secretsmanager"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = [for subnet in aws_subnet.db : subnet.id]
-  security_group_ids  = [aws_security_group.endpoint_sg.id]
-  private_dns_enabled = true
-  tags = {
-    "Name" = "${var.name}-secrets-manager"
-  }
-}
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_endpoint
 resource "aws_vpc_endpoint" "logs" {
-  vpc_id              = aws_vpc.this.id
+  vpc_id              = module.vpc.vpc.id
   service_name        = "com.amazonaws.${var.region}.logs"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = [for subnet in aws_subnet.db : subnet.id]
+  subnet_ids          = [for subnet in module.vpc.private_subnets : subnet.id]
   security_group_ids  = [aws_security_group.endpoint_sg.id]
   private_dns_enabled = true
   tags = {
@@ -43,10 +31,10 @@ resource "aws_vpc_endpoint" "logs" {
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_endpoint
 resource "aws_vpc_endpoint" "ssm" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${var.region}.ssm"
-  vpc_endpoint_type = "Interface"
-  subnet_ids          = [for subnet in aws_subnet.db : subnet.id]
+  vpc_id              = module.vpc.vpc.id
+  service_name        = "com.amazonaws.${var.region}.ssm"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [for subnet in module.vpc.private_subnets : subnet.id]
   security_group_ids  = [aws_security_group.endpoint_sg.id]
   private_dns_enabled = true
   tags = {

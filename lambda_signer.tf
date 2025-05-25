dac9a57717e6a9dcce630baaf48107296b7b494a
Sourav Kundu <skundu.dev@gmail.com>
2025-05-24 19:17:45 -0500

    hypens are not allowed for signing profile

--- lambda_signer.tf
+++ lambda_signer.tf
@@ -1,7 +1,7 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/signer_signing_profile
 resource "aws_signer_signing_profile" "lambda_signing_profile" {
   platform_id = "AWSLambda-SHA384-ECDSA"
-  name        = "${var.name}-lambda-signing-profile"
+  name        = "${replace(var.name, "-", "_")}_lambda_signing_profile"
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_code_signing_config
 resource "aws_lambda_code_signing_config" "configuration" {

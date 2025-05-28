40f15b864cadfff843321c684ba831b1eb8aa135
Sourav Kundu <kunduso@amazon.com>
2025-05-28 08:48:40 -0400

    signer profile names cannot be reused

--- lambda_signer.tf
+++ lambda_signer.tf
@@ -1,7 +1,14 @@
+# Generate a random string to use as a suffix
+#https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/signer_signing_profile
 resource "aws_signer_signing_profile" "lambda_signing_profile" {
   platform_id = "AWSLambda-SHA384-ECDSA"
-  name        = "${replace(var.name, "-", "_")}_lambda_signing_profile"
+  name        = "${replace(var.name, "-", "_")}_lambda_signing_profile_${random_string.suffix.result}"
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_code_signing_config
 resource "aws_lambda_code_signing_config" "configuration" {

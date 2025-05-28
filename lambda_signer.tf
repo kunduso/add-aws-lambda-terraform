15fe914294a6e767727948e2b78559c8b7ebafad
Sourav Kundu <kunduso@amazon.com>
2025-05-28 16:08:02 -0400

    converted to version arn from arn

--- lambda_signer.tf
+++ lambda_signer.tf
@@ -17,13 +17,15 @@ resource "aws_signer_signing_profile" "lambda_signing_profile" {
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_code_signing_config
 resource "aws_lambda_code_signing_config" "configuration" {
   allowed_publishers {
-    signing_profile_version_arns = [aws_signer_signing_profile.lambda_signing_profile.arn]
+    signing_profile_version_arns = [aws_signer_signing_profile.lambda_signing_profile.version_arn]
   }
 
   policies {
     untrusted_artifact_on_deployment = "Enforce"
   }
+  description = "Code signing configuration for ${var.name} Lambda function"
 }
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/signer_signing_job
 resource "aws_signer_signing_job" "build_signing_job" {
   profile_name = aws_signer_signing_profile.lambda_signing_profile.name
 
